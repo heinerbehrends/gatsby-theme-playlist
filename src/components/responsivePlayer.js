6d7b03b120c725dff670f5baaac202bf27aa58aa
heinerbehrends <heinerbehrends@hotmail.com>
2019-12-09 14:17:55 +0100

    Added forwardRef to ResponsivePlayer

--- src/components/responsivePlayer.js
+++ src/components/responsivePlayer.js
@@ -4,30 +4,25 @@ import { jsx } from 'theme-ui';
 import AspectRatioBox from '../components/aspectRatioBox';
 import ReactPlayer from 'react-player';
 
-const ResponsivePlayer = React.forwardRef((props, ref) => {
-  // const refContainer = React.useRef(ReactPlayer);
-  const seekTo = ref.current.seekTo;
-  console.log(seekTo);
-  return (
-    <AspectRatioBox aspectRatio={360 / 640}>
-      <ReactPlayer
-        {...props}
-        ref={ref}
-        sx={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          cursor: 'pointer',
-          backgroundImage: `url(${props.image})`,
-          backgroundSize: 'contain',
-          backgroundPosition: 'center',
-          border: '0.5px solid',
-          borderColor: 'bordercolor',
-          boxShadow: 'boxshadow',
-        }}
-      />
-    </AspectRatioBox>
-  );
-});
+const ResponsivePlayer = React.forwardRef((props, ref) => (
+  <AspectRatioBox aspectRatio={360 / 640}>
+    <ReactPlayer
+      {...props}
+      ref={ref}
+      sx={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        cursor: 'pointer',
+        backgroundImage: `url(${props.image})`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        border: '0.5px solid',
+        borderColor: 'bordercolor',
+        boxShadow: 'boxshadow',
+      }}
+    />
+  </AspectRatioBox>
+));
 
 export default ResponsivePlayer;
